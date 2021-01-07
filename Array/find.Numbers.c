1d6a4cb061f4e35f82ae7c139745c90654f2d730
run-JW <65688438+run-JW@users.noreply.github.com>
2021-01-07 14:38:12 +0900

    Create find.Numbers.c

---
+++ Array/find.Numbers.c
@@ -0,0 +1,21 @@
+int findNumbers(int* nums, int numsSize){
+    int find = 0;
+    int count;
+    int tmp;
+    int i;
+    
+    for(i = 0; i < numsSize; i++) {
+        count = 0;
+        tmp = nums[i];
+        while(tmp != 0)
+        {
+            tmp /= 10;
+            count ++;
+        }
+        
+        if(count % 2 == 0)
+            find++;
+    }
+    
+    return find;
+}
