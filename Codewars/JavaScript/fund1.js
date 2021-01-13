d13715b019565f48ce297abde1e6c8a6dbb11d51
run-JW <65688438+run-JW@users.noreply.github.com>
2021-01-13 23:29:00 +0900

    Update fund1.js

--- Codewars/JavaScript/fund1.js
+++ Codewars/JavaScript/fund1.js
@@ -8,34 +8,3 @@ function toHex(n)
   var result = n.toString(16);
   return result.length == 1 ? "0" + result : result;  
 }
-
-function colorOf(r,g,b)
-{
-   return "#" + toHex(r)+ toHex(g)+ toHex(b);  
-}
-
-function toHex(n)
-{
-  var result = n.toString(16);
-  return result.length == 1 ? "0" + result : result;  
-}
-function colorOf(r,g,b)
-{
-   return "#" + toHex(r)+ toHex(g)+ toHex(b);  
-}
-
-function toHex(n)
-{
-  var result = n.toString(16);
-  return result.length == 1 ? "0" + result : result;  
-}
-function colorOf(r,g,b)
-{
-   return "#" + toHex(r)+ toHex(g)+ toHex(b);  
-}
-
-function toHex(n)
-{
-  var result = n.toString(16);
-  return result.length == 1 ? "0" + result : result;  
-}
