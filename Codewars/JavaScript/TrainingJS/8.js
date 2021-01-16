c04e6cb26d0aee7d3b352456141fb9d5cd278122
run-JW <65688438+run-JW@users.noreply.github.com>
2021-01-17 00:11:49 +0900

    Create 8.js

---
+++ Codewars/JavaScript/TrainingJS/8.js
@@ -0,0 +1,45 @@
+function howManydays(month){
+  var days;
+  switch (month){
+    case 2:
+      days = 28
+      break;
+    case 4:
+      days = 30
+      break;
+    case 6:
+      days = 30
+      break;
+    case 9:
+      days = 30
+      break;
+    case 11:
+      days = 30
+      break;
+    default:
+      days = 31
+  }
+  return days;
+}
+
+/*
+function howManydays(month){
+  switch (month){
+     case 2: return 28
+     case 4:
+     case 6:
+     case 9:
+     case 11: return 30
+  }
+  return 31
+}
+
+function howManydays(month){
+  switch ( month ){
+    case 2 : return 28
+    case 4: case 6: case 9: case 11: return 30
+    default: return 31
+  }
+}
+50 similar code variatio
+*/
