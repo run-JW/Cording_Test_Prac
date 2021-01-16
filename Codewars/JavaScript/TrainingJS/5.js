2b2702afcbb9eb02ee2a7cc283579fff8908375f
run-JW <65688438+run-JW@users.noreply.github.com>
2021-01-16 23:41:07 +0900

    Update 5.js

--- Codewars/JavaScript/TrainingJS/5.js
+++ Codewars/JavaScript/TrainingJS/5.js
@@ -23,3 +23,6 @@ function animal(obj){
 function animal(obj){
   return "This " + obj.color + " " + obj.name + " has " + obj.legs + " legs."
 }
+function animal(obj){
+  return "This " + obj.color + " " + obj.name + " has " + obj.legs + " legs."
+}
