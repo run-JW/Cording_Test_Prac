640f747aeddf97eeb403d60525c3ac129f3f3a69
run-JW <65688438+run-JW@users.noreply.github.com>
2021-01-16 23:29:10 +0900

    Update 5.js

--- Codewars/JavaScript/TrainingJS/5.js
+++ Codewars/JavaScript/TrainingJS/5.js
@@ -2,3 +2,15 @@ function animal(obj){
   return "This " + obj.color + " " + obj.name + " has " + obj.legs + " legs."
 }
 
+/*
+also
+
+function animal(obj){
+  return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
+}
+
+or
+
+const animal = (obj) => `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
+
+*/
