57a5e8f0c648968ba2d7ee657f9381314a986f91
run-JW <65688438+run-JW@users.noreply.github.com>
2021-01-19 23:18:35 +0900

    Update 9.js

--- Codewars/JavaScript/TrainingJS/9.js
+++ Codewars/JavaScript/TrainingJS/9.js
@@ -109,24 +109,4 @@ function padIt(str,n){
     n --;
   }
   return str;
-}function padIt(str,n){
-  while(n>0){
-    if(n%2 === 0) {
-      str = str + "*";
-    }else{
-      str = "*" + str;
-    }
-    n --;
-  }
-  return str;
-}function padIt(str,n){
-  while(n>0){
-    if(n%2 === 0) {
-      str = str + "*";
-    }else{
-      str = "*" + str;
-    }
-    n --;
-  }
-  return str;
 }
