ed2f45439e25257cac11d84319a212356748c202
run-JW <65688438+run-JW@users.noreply.github.com>
2021-01-20 23:47:43 +0900

    Update Array1.js

--- Codewars/JavaScript/Array1.js
+++ Codewars/JavaScript/Array1.js
@@ -9,6 +9,16 @@ Array.prototype.filter = function(fn){
   return res;
 }
 
+Array.prototype.filter = function(fn){
+  var res = [];
+  for (var i = 0; i<this.length; i++){
+    if (fn(this[i])){
+      res.push(this[i]);
+    }
+  }
+  return res;
+}
+
 
 Array.prototype.filter = function(fn){
   var res = [];
