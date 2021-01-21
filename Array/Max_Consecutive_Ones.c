31568f08ce6cccb1633c510c4c209c675b2ed7e5
run-JW <65688438+run-JW@users.noreply.github.com>
2021-01-21 23:53:35 +0900

    Update Max_Consecutive_Ones.c

--- Array/Max_Consecutive_Ones.c
+++ Array/Max_Consecutive_Ones.c
@@ -17,3 +17,22 @@ int findMaxConsecutiveOnes(int* nums, int numsSize){
     }
     return max;
 }
+
+int findMaxConsecutiveOnes(int* nums, int numsSize){
+    int count = 0;
+    int max = 0;
+    int i;
+    
+    // 카운트를 한다.
+    for(i = 0; i < numsSize; i++) {
+        if(nums[i] == 1) {
+            count++;
+            if(count > max)
+                max = count;
+        }
+        if(nums[i] == 0) {
+            count = 0;
+        }
+    }
+    return max;
+}
