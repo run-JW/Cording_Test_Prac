e3495d489f62d47f495195f9ade669c31e05073a
run-JW <65688438+run-JW@users.noreply.github.com>
2021-08-02 11:13:10 +0900

    Update find-path-in-maze.js
    update optimize ver

--- LeetCode/find-path-in-maze.js
+++ LeetCode/find-path-in-maze.js
@@ -34,3 +34,44 @@ const nearestExit = (maze, entrance) => {
 /**
 * Runtime : 292 ms(6.35%) / Memory Usage: 51.6 MB(28.57%)
 */
+
+// Optimize Function
+
+/**
+ * @param {character[][]} maze
+ * @param {number[]} entrance
+ * @return {number}
+ */
+
+class Position {
+    constructor(y, x, distance) {
+        this.row = y;
+        this.column = x;
+        this.distance = distance;
+    }
+}
+
+const nearestExitOptimize = (maze, entrance) => {
+    const directions = [[0, 1], [1, 0], [-1, 0], [0, -1]];
+    const [row, col] = [maze.length, maze[0].length];
+    const queue = [new Position(entrance[0], entrance[1], 0)]
+    maze[entrance[0]][entrance[1]] = '-';
+    
+    while (queue.length) {
+        const dataPoint = queue.shift();
+
+        for (const [dY, dX] of directions) {
+            let [newY, newX] = [dataPoint.row + dY, dataPoint.column + dX];
+            if (newY >= 0 && newX >= 0 && newY < row && newX < col && maze[newY][newX] === '.') {
+                if (newY === 0 || newY === row - 1 || newX === 0 || newX === col - 1) return dataPoint.distance + 1;
+                maze[newY][newX] = '-';
+                queue.push(new Position(newY, newX, dataPoint.distance + 1));
+            }
+        }
+    }
+    return -1;
+};
+
+/**
+* Rumtime 116 ms(94.44%) / Memory Usage 47.5 MB(92.06%)
+*/
