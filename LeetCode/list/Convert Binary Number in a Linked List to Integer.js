8328ecfeba93ea435bf1e81872399727ab894e9c
run-JW <65688438+run-JW@users.noreply.github.com>
2022-02-22 07:28:54 +0900

    Create Convert Binary Number in a Linked List to Integer.js

---
+++ LeetCode/list/Convert Binary Number in a Linked List to Integer.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {number}
+ */
+const getDecimalValue = head => {
+    const result = [];
+    
+    while (head.next !== null) {
+        result.push(head.val);
+        head = head.next;
+    }
+    result.push(head.val);    
+    
+    return parseInt(result.join(''), 2);
+};
